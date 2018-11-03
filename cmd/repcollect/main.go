f1ef0ead3ea3e7f740f909ad783048a98c4878ef
Pascal Gauthier <laghoule@gmail.com>
2018-11-03 02:49:49 +0000

    Add more fonctionality (WIP)

--- cmd/repcollect/main.go
+++ cmd/repcollect/main.go
@@ -11,11 +11,11 @@ import (
 
 func main() {
 	// cmdline args
-	// countPtr := flag.Uint("count", 10, "Number of request to do")
+	countPtr := flag.Uint("count", 10, "Number of request to do")
 	targetPtr := flag.String("target", "https://www.example.com", "Endpoint target")
 	verbosePtr := flag.Bool("verbose", true, "Enable verbose mode")
 	flag.Parse()
 
 	// Call the target and get response time to stdout
-	corelibs.GetBobyResponseTime(*targetPtr, *verbosePtr)
+	corelibs.GetMetrics(*targetPtr, *countPtr, *verbosePtr)
 }
--- internal/pkg/corelibs/corelibs.go
+++ internal/pkg/corelibs/corelibs.go
@@ -4,12 +4,12 @@ package corelibs
 
 import (
 	"fmt"
+	"github.com/tcnksm/go-httpstat"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
-	"github.com/tcnksm/go-httpstat"
 )
 
 // GetBody connect to the http/https target, get the body response, and return
@@ -30,9 +30,56 @@ func GetBody(target string) string {
 	return string(body)
 }
 
-// GetBobyResponseTime connect to http/https target and give response time
+// GetMetrics call getBodyResponse and call getMeanTimes for getting average
+// response time
+func GetMetrics(target string, count uint, verbose bool) {
+
+	// Slice of the metrics, will have len of `count`
+	var metrics []httpstat.Result
+
+	for i := 0; i <= int(count); i++ {
+		metrics = append(metrics, getBobyResponseTime(target, verbose))
+	}
+
+	// Work in progress
+	// Will return mean time in httpstat.Result type
+	getMeanTimes(metrics)
+}
+
+// getMeanTimes collect metrics and return average response times
+// Remove the highest and lowest value of each dataset (for more acuracy?)
+func getMeanTimes(metrics []httpstat.Result) {
+
+	var (
+		DNSLookup        time.Duration
+		TCPConnection    time.Duration
+		TLSHandshake     time.Duration
+		ServerProcessing time.Duration
+		//ContentTransfer time.Duration
+		//Total time.Duration
+	)
+
+	for _, metric := range metrics {
+		DNSLookup += metric.DNSLookup
+		TCPConnection += metric.TCPConnection
+		TLSHandshake += metric.TLSHandshake
+		ServerProcessing += metric.ServerProcessing
+		//ContentTransfer += metric.ContentTransfer(time.Now())
+		//Total += metric.Total(time.Now())
+	}
+
+	fmt.Printf("Mean DNS: %d ", int(DNSLookup/time.Millisecond)/len(metrics))
+	fmt.Printf("TCPConnection: %d ", int(TCPConnection/time.Millisecond)/len(metrics))
+	fmt.Printf("TLSHandshake: %d ", int(TLSHandshake/time.Millisecond)/len(metrics))
+	fmt.Printf("ServerProcessing: %d ", int(ServerProcessing/time.Millisecond)/len(metrics))
+	//fmt.Printf("ContentTransfer: %d ", int(ContentTransfer(time.Now())/time.Millisecond)/len(metrics))
+	//fmt.Printf("Total: %d", int(Total(time.Now())/time.Millisecond)/len(metrics))
+}
+
+// getBobyResponseTime connect to http/https target and give response time
 // Based on https://medium.com/@deeeet/trancing-http-request-latency-in-golang-65b2463f548c
-func GetBobyResponseTime(target string, verbose bool) {
+func getBobyResponseTime(target string, verbose bool) httpstat.Result {
+
 	// Create a new HTTP request
 	req, err := http.NewRequest("GET", target, nil)
 	if err != nil {
@@ -43,6 +90,7 @@ func GetBobyResponseTime(target string, verbose bool) {
 	var result httpstat.Result
 	ctx := httpstat.WithHTTPStat(req.Context(), &result)
 	req = req.WithContext(ctx)
+	req.Close = true
 
 	// Send request by default HTTP client
 	client := http.DefaultClient
@@ -63,6 +111,8 @@ func GetBobyResponseTime(target string, verbose bool) {
 		fmt.Printf("TLS handshake: %d ms\n", int(result.TLSHandshake/time.Millisecond))
 		fmt.Printf("Server processing: %d ms\n", int(result.ServerProcessing/time.Millisecond))
 		fmt.Printf("Content transfer: %d ms\n", int(result.ContentTransfer(time.Now())/time.Millisecond))
-		fmt.Printf("Total time of the transaction: %d ms\n", int(result.Total(time.Now())/time.Millisecond))
+		fmt.Printf("Total time of the transaction: %d ms\n\n", int(result.Total(time.Now())/time.Millisecond))
 	}
+
+	return result
 }
