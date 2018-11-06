408c0c71eda43f8fb84a2db68e964cbfa4ccba0f
Pascal Gauthier <laghoule@gmail.com>
2018-11-06 01:55:01 +0000

    gofmt

--- internal/pkg/corelibs/sqs.go
+++ internal/pkg/corelibs/sqs.go
@@ -5,6 +5,6 @@ import (
 )
 
 // transformToJSON will transform metric to json format
-func sendMetricToSQS (metric HTTPMetric) {
+func sendMetricToSQS(metric HTTPMetric) {
 	fmt.Println("will send metric to SQS queue")
 }
