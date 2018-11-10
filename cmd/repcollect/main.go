0d3c9a6073f491100af8f0bce34bc325fac553fc
Pascal Gauthier <laghoule@gmail.com>
2018-11-10 22:04:20 +0000

    Remove most of cmdline flag and replace by config file

--- cmd/repcollect/main.go
+++ cmd/repcollect/main.go
@@ -5,20 +5,20 @@ Here will be a description of the tools
 package main
 
 import (
-	"fmt"
 	"flag"
 	"github.com/laghoule/reptime/internal/pkg/corelibs"
 )
 
 func main() {
 	// cmdline args
-	countPtr := flag.Uint("count", 5, "Number of request to do")
-	targetPtr := flag.String("target", "https://www.example.com", "Endpoint target")
-	verbosePtr := flag.Bool("verbose", false, "Enable verbose mode")
+	verbosePtr := flag.Bool("verbose", true, "Enable verbose mode")
 	configFilePtr := flag.String("config", "/etc/reptime/repcollect.conf", "configuration file")
 	flag.Parse()
 
 	// Call the target and get response time to stdout
-	fmt.Println(corelibs.LoadConfig(*configFilePtr))
-	corelibs.GetMetrics(*targetPtr, *countPtr, *verbosePtr)
+	config:= corelibs.LoadConfig(*configFilePtr)
+	for _, target := range config.Targets {
+		corelibs.GetMetrics(config.Protocol+"://"+target, config.Count, *verbosePtr)
+	}
+	
 }
