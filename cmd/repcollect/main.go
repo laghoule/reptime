d90cefb308750b0bca349f17bad650b19d63872f
Pascal Gauthier <laghoule@gmail.com>
2018-11-10 20:24:18 +0000

    Begin integration of config file

--- cmd/repcollect/main.go
+++ cmd/repcollect/main.go
@@ -5,6 +5,7 @@ Here will be a description of the tools
 package main
 
 import (
+	"fmt"
 	"flag"
 	"github.com/laghoule/reptime/internal/pkg/corelibs"
 )
@@ -13,9 +14,11 @@ func main() {
 	// cmdline args
 	countPtr := flag.Uint("count", 5, "Number of request to do")
 	targetPtr := flag.String("target", "https://www.example.com", "Endpoint target")
-	verbosePtr := flag.Bool("verbose", true, "Enable verbose mode")
+	verbosePtr := flag.Bool("verbose", false, "Enable verbose mode")
+	configFilePtr := flag.String("config", "/etc/reptime/repcollect.conf", "configuration file")
 	flag.Parse()
 
 	// Call the target and get response time to stdout
+	fmt.Println(corelibs.LoadConfig(*configFilePtr))
 	corelibs.GetMetrics(*targetPtr, *countPtr, *verbosePtr)
 }
