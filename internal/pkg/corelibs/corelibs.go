c554841788cec669b41a910b47002683b425b4e1
Pascal Gauthier <laghoule@gmail.com>
2018-11-11 01:07:02 +0000

    Move to repcollect main

--- internal/pkg/corelibs/corelibs.go
+++ internal/pkg/corelibs/corelibs.go
@@ -13,20 +13,3 @@ type HTTPMetric struct {
 	contentTransfer  time.Duration
 	totalTime        time.Duration
 }
-
-// AwsConfig contain creds and info for AWS
-type AwsConfig struct {
-	AccessKey string
-	SecretKey string
-	Region    string
-	QueueURL  string
-}
-
-// Config struct
-type Config struct {
-	Targets  []string
-	Protocol string
-	Count    int
-	Timeout  int
-	AwsConfig
-}
