d799110a29f3886617a3fbef8ea0a3ef47759335
Pascal Gauthier <laghoule@gmail.com>
2018-11-15 02:56:44 +0000

    Begin to add config error handler

--- cmd/repcollect/config.go
+++ cmd/repcollect/config.go
@@ -7,6 +7,18 @@ import (
 	"strings"
 )
 
+// LoadConfigError is a error handler struct
+type LoadConfigError struct {
+	filename string
+	section  string
+	key      string
+}
+
+// Error handler
+func (err *LoadConfigError) Error() string {
+	return fmt.Sprintf("Error loading configuration file %s (section: %s | key: %s)", err.filename, err.section, err.key)
+}
+
 // LoadConfig from configuration file
 func LoadConfig(configFile string) Config {
 	var config Config
@@ -17,6 +29,7 @@ func LoadConfig(configFile string) Config {
 		os.Exit(1)
 	}
 
+	// TODO: Add config checker
 	config.AccessKey = cfgfile.Section("aws").Key("access_key").String()
 	config.SecretKey = cfgfile.Section("aws").Key("secret_key").String()
 	config.Region = cfgfile.Section("aws").Key("region").MustString("us-east-1")
